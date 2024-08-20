bef62b355d070a708e6d0aee51ce520a8f99f48e
nova27 <33594303+nova-27@users.noreply.github.com>
2024-08-21 03:52:42 +0900

    update: support pop

--- hackvm/instruction_parser.py
+++ hackvm/instruction_parser.py
@@ -1,3 +1,6 @@
+SYMBOLS = {'argument': 'ARG', 'local': 'LCL', 'this': 'THIS', 'that': 'THAT'}
+
+
 class InstructionParser:
     sp_inst: list[str]
 
@@ -7,17 +10,73 @@ class InstructionParser:
     def convert(self, label_id: int) -> (list[str], int):
         opcode = self.sp_inst[0]
         if opcode == 'push':
-            num = self.sp_inst[2]
-            return ([
-                f'@{num}',
-                'D=A',
+            seg = self.sp_inst[1]
+            num = int(self.sp_inst[2])
+
+            asm: list[str]
+            if seg == 'constant':
+                asm = [
+                    f'@{num}',
+                    'D=A'
+                ]
+            elif seg in SYMBOLS:
+                asm = [
+                    f'@{num}',
+                    'D=A',
+                    f'@{SYMBOLS[seg]}',
+                    'A=D+M',
+                    'D=M'
+                ]
+            elif seg == 'temp':
+                asm = [
+                    f'@{5 + num}',
+                    'D=M'
+                ]
+            else:
+                raise SyntaxError(f'unknown segment: {seg}')
+
+            asm.extend([
                 '@SP',
                 'A=M',
                 'M=D',
                 'D=A+1',
                 '@SP',
                 'M=D'
-            ], label_id)
+            ])
+
+            return asm, label_id
+        elif opcode == 'pop':
+            seg = self.sp_inst[1]
+            num = int(self.sp_inst[2])
+
+            asm: list[str]
+            if seg in SYMBOLS:
+                asm = [
+                    f'@{num}',
+                    'D=A',
+                    f'@{SYMBOLS[seg]}',
+                    'D=D+M'
+                ]
+            elif seg == 'temp':
+                asm = [
+                    f'@{5 + num}',
+                    'D=A'
+                ]
+            else:
+                raise SyntaxError(f'unknown segment: {seg}')
+
+            asm.extend([
+                '@R13',
+                'M=D',
+                '@SP',
+                'AM=M-1',
+                'D=M',
+                '@R13',
+                'A=M',
+                'M=D'
+            ])
+
+            return asm, label_id
         elif opcode in ('add', 'sub', 'and', 'or'):
             exp = {'add': 'D+M', 'sub': 'M-D', 'and': 'D&M', 'or': 'D|M'}
             return ([
