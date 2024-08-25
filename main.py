4b1f24ff0e43169907aec54c169108922796f2c3
nova27 <33594303+nova-27@users.noreply.github.com>
2024-08-25 23:44:20 +0900

    add: support multiple source files

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import sys
 import hackvm
 
 if __name__ == '__main__':
-    vm_filename = sys.argv[1]
-    vm_file = open(vm_filename, 'r')
-    converter = hackvm.VMConverter(vm_file.readlines())
+    vm_code = []
+    for vm_filename in sys.argv[1:]:
+        vm_file = open(vm_filename, 'r')
+        vm_code.extend(vm_file.readlines())
 
-    export_filename = vm_filename[:vm_filename.rfind('.')] + '.asm'
+    converter = hackvm.VMConverter(vm_code)
+    export_filename = 'out.asm'
     export_file = open(export_filename, 'w')
     export_data = '\n'.join(converter.convert_all())
     export_file.write(export_data)
